210efb1577c7df45cca363a762589492f16e0c74
ylwn29 <ylwn29@gmail.com>
2023-11-10 02:07:41 +0000

    add semwait sempost cleanup function and edit variables in header

--- Project3/stairs.c
+++ Project3/stairs.c
@@ -9,13 +9,28 @@ int downCount = 0;
 // You can write your own semwait function that can call sem_wait(sem) or sem_trywait(sem)
 // in addition to checking/setting proper variables
 // properly use pthread_mutex_lock/unlock
-
+void semwait(sem_t *sem) {
+    pthread_mutex_lock(&mutex);
+    if (on_stairs >= MAX_CUS_ON_STAIRS) {
+        pthread_mutex_unlock(&mutex);
+        sem_wait(sem);
+    } else {
+        on_stairs++;
+        pthread_mutex_unlock(&mutex);
+    }
+}
 
 // You can write your own sempost function that call sem_post(sem)
 // in addition to checking/setting proper variables
 // properly use pthread_mutex_lock/unlock
-
-
+void sempost(sem_t *sem) {
+    pthread_mutex_lock(&mutex);
+    on_stairs--;
+    if (on_stairs == 0) {
+        sem_post(sem) // Signal that the stairs are free
+    }
+    pthread_mutex_unlock(&mutex);
+}
 
 void *threadfunction (void *vargp) {
     // write the threadfunction whose name should be part of pthread_create(..)
@@ -61,6 +76,13 @@ void *threadfunction (void *vargp) {
     // calculate turnaround time
 
     pthread_exit(NULL);
+}
+
+// function to clean up thread, mutex and semaphore
+void cleanup() {
+    free(tid);
+    pthread_mutex_destroy(&mutex);
+    sem_destroy(&sem);
 }
 
 int main(int argc, char *argv[]) {
--- Project3/stairs.h
+++ Project3/stairs.h
@@ -21,24 +21,32 @@ typedef struct thread_arg {
 
 // #define the maximum number of customers/threads in the system to test
 int MAX_CUSTOMERS = 30;
+// define the maximum number of steps(or stairs)
+int MAX_STAIRS = 13;
 // #define how many customer can be on the stairs at the same time
-int CUS_ON_STAIR = 1;
+int MAX_CUS_ON_STAIRS = 1;
 // you can also define other constants for your "prevent deadlock" or "prevent starvation" algorithm
 
-// define timing variables
 
+// define timing variables
 
 
 // thread
 pthread_t *tid;
 
-
-
 // Define global variables on the allowed direction, waiting threads, ...
 
-
+// initialize the mutex with the constant which is provided by pthread.h library
+// to set all the fields of the mutex attributes structure to default values
 pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
+// initialize a semaphore
 sem_t sem;
+// define the number of customers currently on the stairs
+int on_stairs = 0;
 
 
 // write any helper functions you need here
+void semwait(sem_t *sem);
+void sempost(sem_t *sem);
+void *threadfunction(void *vargp);
+void cleanup();
